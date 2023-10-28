017a4800e38037a74e603ad028dee5c012965359
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2023-10-28 19:14:23 +0100

    deleted old main-branche code starting from scratch to make the best drive straight possible

--- src/main.h
+++ src/main.h
@@ -1,13 +1,5 @@
 #ifndef MAIN_PROG
 #define MAIN_PROG
 
-#include "SoftwareTimer.h"
-#include "PIDController.h"
-#include "MotorController.h"
 
-template <MotorController& mott>
-void readEncoder();
-void setLeftMotorTargetRPM(int rpm);
-void setRightMotorTargetRPM(int rpm);
-int CMtoPulses(float cm);
 #endif
